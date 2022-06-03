bc9b0b0a4a4675163bb939583d478af3d4650144
HugePaint <iyyh@qq.com>
2022-06-03 00:37:39 -0400

    Test regression model

--- ml_process.py
+++ ml_process.py
@@ -1,5 +1,7 @@
 import json
 import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.colors import ListedColormap
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.datasets import make_moons, make_circles, make_classification
@@ -40,7 +42,7 @@ classifiers = [
     QuadraticDiscriminantAnalysis(),
 ]
 
-# TODO: flip male/female and append them to X_train
+# flip male/female and append them to X_train
 # TODO: try use counts instead of frequency
 # additional feature: word count, highest fequency of a full name
 # TODO: try regression 
@@ -53,6 +55,7 @@ feature_list = list()
 for a in article_list:
     segment_words = a["word_count"] // a["segments"]
     features = [a["male_person"] - a["female_person"]]
+    features.append(a["word_count"])
     for segment in a["segmented_gender_word_count"]:
         for x in segment:
             features.append(x / segment_words)
@@ -61,47 +64,98 @@ for a in article_list:
     # flip
     segment_words = a["word_count"] // a["segments"]
     features = [a["female_person"] - a["male_person"]]
+    features.append(a["word_count"])
     for segment in a["segmented_gender_word_count"]:
         features.append(segment[1] / segment_words)
         features.append(segment[0] / segment_words)
     feature_list.append(features)
-# print(feature_list)
 
-female_results = list()
-male_results = list()
+
+expect_results_with_flipped_data = list()
 for e in expect_results.values():
     # for each e:
     # ["Female Central (out of 5) - ZY", "Male Central (out of 5) - ZY"]
-    female_results.append(e[0])
-    male_results.append(e[1])
+    expect_results_with_flipped_data.append(e[0])
 
-    # flip
-    female_results.append(e[1])
-    male_results.append(e[0])
+    # add flipped male score to the y set
+    expect_results_with_flipped_data.append(e[1])
 
 
-X = feature_list
-y = male_results
 
 # cross validation
 # Ref: https://scikit-learn.org/stable/modules/cross_validation.html#cross-validation
-# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.4, random_state=0)
-# clf = RandomForestClassifier(max_depth=5, n_estimators=10, max_features=1).fit(X_train, y_train)
-# print(clf.score(X_test, y_test))
 
-y = male_results
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.4, random_state=0)
+X = np.array(feature_list)
+y = np.array(expect_results_with_flipped_data)
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.4, random_state=0
+)
+
+x_min, x_max = min([i[0] for i in X]) - 0.5, max([i[0] for i in X]) + 0.5
+y_min, y_max = min([i[1] for i in X]) - 0.5, max([i[1] for i in X]) + 0.5
+
+# just plot the dataset first
+cm = plt.cm.RdBu
+cm_bright = ListedColormap(["#FF0000", "#0000FF"])
+ax = plt.subplot(1, len(classifiers) + 1, 1)
+ax.set_title("Input data")
+# Plot the training points
+# ax.scatter([i[0] for i in X_train], [i[1] for i in X_train], c=y_train, cmap=cm_bright, edgecolors="k")
+ax.scatter(X_train[:, 0], X_train[:, 1], c=y_train, cmap=cm_bright, edgecolors="k")
+# Plot the testing points
+# ax.scatter(
+#     [i[0] for i in X_test], [i[1] for i in X_test], c=y_test, cmap=cm_bright, alpha=0.6, edgecolors="k"
+# )
+ax.scatter(
+        X_test[:, 0], X_test[:, 1], c=y_test, cmap=cm_bright, alpha=0.6, edgecolors="k"
+)
+ax.set_xlim(x_min, x_max)
+ax.set_ylim(y_min, y_max)
+ax.set_xticks(())
+ax.set_yticks(())
+
+i = 1
 print("--------Testing male set")
 for name, clf in zip(names, classifiers):
     clf.fit(X_train, y_train)
     score = clf.score(X_test, y_test)
     print(name + ": " + str(score))
 
-print("\n")
-y = female_results
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.4, random_state=0)
-print("---------Testing female set")
-for name, clf in zip(names, classifiers):
-    clf.fit(X_train, y_train)
-    score = clf.score(X_test, y_test)
-    print(name + ": " + str(score))
+    
+#     ax = plt.subplot(1, len(classifiers) + 1, i)
+#     clf.fit(X_train, y_train)
+#     score = clf.score(X_test, y_test)
+#     # DecisionBoundaryDisplay.from_estimator(
+#     #     clf, X, cmap=cm, alpha=0.8, ax=ax, eps=0.5
+#     # )
+
+#     # Plot the training points
+#     ax.scatter(
+#             X_train[:, 0], X_train[:, 1], c=y_train, cmap=cm_bright, edgecolors="k"
+#     )
+#     # Plot the testing points
+#     ax.scatter(
+#             X_test[:, 0],
+#             X_test[:, 1],
+#             c=y_test,
+#             cmap=cm_bright,
+#             edgecolors="k",
+#             alpha=0.6,
+#     )
+
+#     ax.set_xlim(x_min, x_max)
+#     ax.set_ylim(y_min, y_max)
+#     ax.set_xticks(())
+#     ax.set_yticks(())
+#     ax.set_title(name)
+#     ax.text(
+#         x_max - 0.3,
+#         y_min + 0.3,
+#         ("%.2f" % score).lstrip("0"),
+#         size=15,
+#         horizontalalignment="right",
+#     )
+#     i += 1
+
+# plt.tight_layout()
+# plt.show()
---
+++ ml_process_regression.py
@@ -0,0 +1,90 @@
+import json
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.colors import ListedColormap
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import AdaBoostRegressor
+from sklearn.tree import DecisionTreeRegressor
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.metrics import r2_score
+
+# flip male/female and append them to X_train
+# TODO: try use counts instead of frequency
+# additional feature: word count, highest fequency of a full name
+# TODO: try regression 
+
+article_list = json.load(open('article_list.json',))
+expect_results = json.load(open('expect_results.json',))
+
+feature_list = list()
+# get feature from extracted list
+for a in article_list:
+    segment_words = a["word_count"] // a["segments"]
+    features = [a["male_person"] - a["female_person"]]
+    features.append(a["word_count"])
+    for segment in a["segmented_gender_word_count"]:
+        for x in segment:
+            features.append(x / segment_words)
+    feature_list.append(features)
+
+    # flip
+    segment_words = a["word_count"] // a["segments"]
+    features = [a["female_person"] - a["male_person"]]
+    features.append(a["word_count"])
+    for segment in a["segmented_gender_word_count"]:
+        features.append(segment[1] / segment_words)
+        features.append(segment[0] / segment_words)
+    feature_list.append(features)
+
+
+expect_results_with_flipped_data = list()
+for e in expect_results.values():
+    # for each e:
+    # ["Female Central (out of 5) - ZY", "Male Central (out of 5) - ZY"]
+    expect_results_with_flipped_data.append(e[0])
+
+    # add flipped male score to the y set
+    expect_results_with_flipped_data.append(e[1])
+
+
+
+# cross validation
+# Ref: https://scikit-learn.org/stable/modules/cross_validation.html#cross-validation
+
+X = np.array(feature_list)
+y = np.array(expect_results_with_flipped_data)
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.4, random_state=0
+)
+
+# Decision Tree Regression with AdaBoost
+# Ref: https://scikit-learn.org/stable/auto_examples/ensemble/plot_adaboost_regression.html
+
+regr_1 = DecisionTreeRegressor(max_depth=4)
+
+regr_2 = AdaBoostRegressor(
+    DecisionTreeRegressor(max_depth=4), n_estimators=300, random_state=None
+)
+
+regr_1.fit(X_train, y_train)
+regr_2.fit(X_train, y_train)
+
+y_1 = regr_1.predict(X_test)
+y_2 = regr_2.predict(X_test)
+
+print(r2_score(y_test, y_1))
+print(r2_score(y_test, y_2))
+
+
+colors = sns.color_palette("colorblind")
+
+plt.figure()
+plt.scatter(X_test[:,0], y_test, color=colors[0], label="training samples")
+plt.plot(X_test[:,0], y_1, color=colors[1], label="n_estimators=1", linewidth=2)
+plt.plot(X_test[:,0], y_2, color=colors[2], label="n_estimators=300", linewidth=2)
+plt.xlabel("data")
+plt.ylabel("target")
+plt.title("Boosted Decision Tree Regression")
+plt.legend()
+plt.show()
