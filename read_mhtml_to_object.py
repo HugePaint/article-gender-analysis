14f3594f970ee4855add755a84b1b47806cb58a2
HugePaint <iyyh@qq.com>
2022-07-06 01:14:27 -0400

    Update read_mhtml_to_object.py
    Select a folder, then the script will search for all mhtml files and generate a json file containing every Article object in the same directory as each mhtml

--- read_mhtml_to_object.py
+++ read_mhtml_to_object.py
@@ -6,6 +6,10 @@ import quopri
 import re
 import json
 import dataclasses
+import os
+import numpy as np
+import tkinter as tk
+from tkinter import filedialog
 
 class EnhancedJSONEncoder(json.JSONEncoder):
         def default(self, o):
@@ -21,60 +25,81 @@ class EnhancedJSONEncoder(json.JSONEncoder):
 
 # pp.install_extras()
 
-file_path = r"data\Australia_Corp_2003.mhtml"
-with open(file_path, 'r') as fp:
-    new_soup = BeautifulSoup(quopri.decodestring(fp.read()), 'lxml')
-    article_nodes = new_soup.find_all("div", class_="article", id=re.compile(r'article-[.]*'))
+root = tk.Tk()
+root.withdraw()
+folder_selected = filedialog.askdirectory()
 
-    count = 0
-    article_list = list()
-    for a in article_nodes:
-        count += 1
-        article_object = analyzer.Article(list(), list(), dict())
-        article_object.id = a['id']
+file_path_list = list()
+for root, dirs, files in os.walk(folder_selected):
+    for file in files:
+        if file.endswith(".mhtml"):
+             file_path_list.append(os.path.join(root, file))
 
-        heading_str = a.find("b", string="HD").parent.next_sibling.string
-        article_object.title = heading_str
+for file_path in file_path_list:
+    print("Processing " + file_path)
+    json_out_path = os.path.splitext(file_path)[0]+'.json'
 
-        word_count_str = a.find("b", string="WC").parent.next_sibling.string
-        article_object.word_count = [int(s) for s in word_count_str.split() if s.isdigit()][0]
+    if os.path.exists(json_out_path):
+        print(os.path.basename(json_out_path) + " is exist. Skipped.")
+        continue
 
-        publish_date_str = a.find("b", string="PD").parent.next_sibling.string
-        article_object.year = publish_date_str[-4:]
+    with open(file_path, 'r') as fp:
+        new_soup = BeautifulSoup(quopri.decodestring(fp.read()), 'lxml')
+        article_nodes = new_soup.find_all("div", class_="article", id=re.compile(r'article-[.]*'))
 
-        try:
-            LP_node = a.find("b", string="LP").parent.parent.next_sibling
-            for child in LP_node.children:
-                article_object.text += child.text + " "
-        except:
-            print(article_object.id + ": No LP section")
-        
-        try:
-            TD_node = a.find("b", string="TD").parent.parent.next_sibling
-            for child in TD_node.children:
-                article_object.text += child.text + " "
-        except:
-            print(article_object.id + ": No TD section")
-        
-        article_object.text = article_object.text.replace('\n', '')
+        count = 0
+        article_list = list()
+        for a in article_nodes:
+            count += 1
+            article_object = analyzer.Article(list(), list(), dict())
+            article_object.id = a['id']
 
-        article_list.append(article_object)
+            heading_str = a.find("b", string="HD").parent.next_sibling.string
+            article_object.title = heading_str
 
-        # pp.pprint(article_object, indent=1)
-        print(article_object.id + "/t(" + str(count) + "/" + str(len(article_nodes)) + ")")
+            word_count_str = a.find("b", string="WC").parent.next_sibling.string
+            article_object.word_count = [int(s) for s in word_count_str.split() if s.isdigit()][0]
 
-    for a in article_list:
-        analyzer.analyze(a)
-        print(a.id)
-        # clean up
-        a.text = ""
-        a.text_tagged = list()
+            publish_date_str = a.find("b", string="PD").parent.next_sibling.string
+            article_object.year = publish_date_str[-4:]
 
-    # Serializing json 
-    json_articles = json.dumps(article_list, indent = 4, cls=EnhancedJSONEncoder)
-    with open("data\Australia_Corp_2003.json", "w") as outfile:
-        outfile.write(json_articles)
+            try:
+                LP_node = a.find("b", string="LP").parent.parent.next_sibling
+                for child in LP_node.children:
+                    article_object.text += child.text + " "
+            except:
+                print(article_object.id + ": No LP section")
+            
+            try:
+                TD_node = a.find("b", string="TD").parent.parent.next_sibling
+                for child in TD_node.children:
+                    article_object.text += child.text + " "
+            except:
+                print(article_object.id + ": No TD section")
+            
+            article_object.text = article_object.text.replace('\n', '')
+
+            article_list.append(article_object)
+
+            # pp.pprint(article_object, indent=1)
+            print("Article Object: " + article_object.id + " (" + str(count) + "/" + str(len(article_nodes)) + ")")
+
+        count = 0
+        for a in article_list:
+            count += 1
+            analyzer.analyze(a)
+            print("Analyzing " + a.id + " (" + str(count) + "/" + str(len(article_nodes)) + ")")
+            # clean up
+            a.text = ""
+            a.text_tagged = list()
+
+        # Serializing json 
+        json_articles = json.dumps(article_list, indent = 4, cls=EnhancedJSONEncoder)
+        with open(json_out_path, "w") as outfile:
+            outfile.write(json_articles)
+            print("Dumped:  " + json_out_path)
 
 
-    print(article_nodes)
+
+
 
